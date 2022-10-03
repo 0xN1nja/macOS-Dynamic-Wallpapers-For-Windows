b5e027b47f3d54bcba8698a4b1a8647aa120a280
Abhimanyu Sharma <71812498+0xN1nja@users.noreply.github.com>
2022-10-03 19:51:45 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# macOS Dynamic Wallpapers For Windows (Python)
+## Automatically Changes Desktop's Wallpaper Based On Time ⏰
+![wallpaper-changing](https://raw.githubusercontent.com/0xN1nja/macOS-Dynamic-Wallpapers-For-Windows/master/assets/wallpaper_changing.gif)
+# Installation
+### Install Python ✅
+https://www.python.org/downloads/
+### Clone This Repository ✅
+```
+git clone https://github.com/0xN1nja/macOS-Dynamic-Wallpapers-For-Windows.git
+```
+### Or Download This Repository As A Zip File 📁
+https://github.com/0xN1nja/macOS-Dynamic-Wallpapers-For-Windows/archive/refs/heads/master.zip
+# Run
+### Select "Span" As A Wallpaper Fit In Settings For A Better Experience (Optional)
+![span-fit](https://raw.githubusercontent.com/0xN1nja/macOS-Dynamic-Wallpapers-For-Windows/master/assets/choose_a_fit.png)
+### Open Command Prompt/Powershell/Terminal/Shell And Run The Following Command 👇
+```bash
+pythonw main.py
+```
---
+++ main.py
@@ -0,0 +1,36 @@
+import ctypes
+import os
+import datetime
+import time
+
+
+def change_wallpaper(code):
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, os.path.join(os.getcwd(), "wallpapers", f"{code}.jpg"), 3)
+
+
+def check_time_and_change_wallpaper():
+    current_time = datetime.datetime.now().hour
+    if current_time >= 5 and current_time < 6:
+        change_wallpaper("1")
+    elif current_time >= 6 and current_time < 9:
+        change_wallpaper("2")
+    elif current_time >= 9 and current_time < 12:
+        change_wallpaper("3")
+    elif current_time >= 12 and current_time < 16:
+        change_wallpaper("4")
+    elif current_time >= 16 and current_time < 18:
+        change_wallpaper("5")
+    elif current_time >= 18 and current_time < 20:
+        if datetime.datetime.now().minute >= 30:
+            change_wallpaper("6")
+        else:
+            change_wallpaper("5")
+    else:
+        change_wallpaper("7")
+
+
+if __name__ == '__main__':
+    check_time_and_change_wallpaper()
+    while True:
+        check_time_and_change_wallpaper()
+        time.sleep(5)
